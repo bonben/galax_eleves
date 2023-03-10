a42be4bccd6bf25f97a6d7406b0a2cad138ad047
ArthyD <arthur.docquois@wanadoo.fr>
2023-03-10 14:25:13 +0100

    XSIMD first commit

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -10,8 +10,9 @@
 #define SERIAL 0
 #define PARFOR_NAIVE 1
 #define SERIAL_IMPROVED 2
+#define XSIMD 3
 
-#define STRATEGY PARFOR_NAIVE
+#define STRATEGY XSIMD
 
 namespace xs = xsimd;
 using b_type = xs::batch<float, xs::avx2>;
@@ -23,6 +24,7 @@ Model_CPU_fast
 : Model_CPU(initstate, particles)
 {
 }
+
 void Model_CPU_fast
 ::step()
 {
@@ -156,6 +158,52 @@ for (int i = 0; i < n_particles; i ++)
 		}
 	}
 #pragma omp parallel for
+	for (int i = 0; i < n_particles; i++)
+	{
+		velocitiesx[i] += accelerationsx[i] * 2.0f;
+		velocitiesy[i] += accelerationsy[i] * 2.0f;
+		velocitiesz[i] += accelerationsz[i] * 2.0f;
+		particles.x[i] += velocitiesx   [i] * 0.1f;
+		particles.y[i] += velocitiesy   [i] * 0.1f;
+		particles.z[i] += velocitiesz   [i] * 0.1f;
+	}
+
+#elif STRATEGY == XSIMD
+    for (int i = 0; i < n_particles; i += b_type::size)
+    {
+        // load registers body i
+        const b_type rposx_i = b_type::load_unaligned(&particles.x[i]);
+        const b_type rposy_i = b_type::load_unaligned(&particles.y[i]);
+        const b_type rposz_i = b_type::load_unaligned(&particles.z[i]);
+		b_type raccx_i = b_type::load_unaligned(&accelerationsx[i]);
+		b_type raccy_i = b_type::load_unaligned(&accelerationsy[i]);
+        b_type raccz_i = b_type::load_unaligned(&accelerationsz[i]);
+		std::vector<float> once(b_type::size,1.0);
+		b_type once_v = b_type::load_unaligned(&once[0]);
+
+
+        for(int j=0; j<n_particles; j ++){
+
+			b_type diffx = particles.x[j] - rposx_i;
+			b_type diffy = particles.y[j] - rposy_i;
+			b_type diffz = particles.z[j] - rposz_i;	
+
+			b_type dij = diffx * diffx + diffy * diffy + diffz * diffz;	
+
+			dij = xs::max(once_v,dij); //Il faut un batch de 1
+			dij = 10.0 * xs::rsqrt(dij) * xs::rsqrt(dij) * xs::rsqrt(dij);
+
+			raccx_i = raccx_i + diffx * dij * initstate.masses[j];
+			raccy_i = raccy_i + diffy * dij * initstate.masses[j];
+			raccz_i = raccz_i + diffy * dij * initstate.masses[j];
+
+			raccx_i.store_unaligned(&accelerationsx[i]);
+			raccy_i.store_unaligned(&accelerationsy[i]);
+			raccz_i.store_unaligned(&accelerationsz[i]);
+
+		}
+    }
+
 	for (int i = 0; i < n_particles; i++)
 	{
 		velocitiesx[i] += accelerationsx[i] * 2.0f;
