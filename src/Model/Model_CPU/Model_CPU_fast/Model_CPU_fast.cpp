766e4e87dc5976e7a41a305b1b592d8271d1aefa
Aurelien <aurelien.gosse@imt-atlantique.net>
2023-03-10 11:20:27 +0100

    inverse sqare root

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -5,15 +5,13 @@
 #include "Model_CPU_fast.hpp"
 #include <xsimd/xsimd.hpp>
 #include <omp.h>
+#include <immintrin.h>
 
 #define SERIAL 0
 #define PARFOR_NAIVE 1
 #define SERIAL_IMPROVED 2
-#define PARFOR_ATOMIC 3
-#define PARFOR_REDUCE 4
-#define STD_ATOMIC 5
 
-#define STRATEGY SERIAL_IMPROVED
+#define STRATEGY PARFOR_NAIVE
 
 namespace xs = xsimd;
 using b_type = xs::batch<float, xs::avx2>;
@@ -92,9 +90,13 @@ for (int i = 0; i < n_particles; i ++)
 				}
 				else
 				{
-					dij = std::sqrt(dij);
-					dij = 10.0 / (dij * dij * dij);
+					__m256 x_vec = _mm256_set1_ps(dij); // Passage en vecteur AVX
+					__m256 inv_sqrt_x_vec = _mm256_rsqrt_ps(x_vec); // Calcul de l'inverse de la racine carrée
+					inv_sqrt_x_vec = _mm256_mul_ps(inv_sqrt_x_vec,_mm256_mul_ps(inv_sqrt_x_vec,inv_sqrt_x_vec));// Multiplication
+					float inv_sqrt_x = _mm256_cvtss_f32(inv_sqrt_x_vec);//reconversion en float
+					dij = 10.0 * inv_sqrt_x;
 				}
+				
 				accelerationsx[i] += diffx * dij * initstate.masses[j];
 				accelerationsy[i] += diffy * dij * initstate.masses[j];
 				accelerationsz[i] += diffz * dij * initstate.masses[j];
@@ -121,7 +123,7 @@ for (int i = 0; i < n_particles; i ++)
 #pragma omp parallel for
     for (int i = 0; i < n_particles; i ++)
     {
-        for (int j = 0; j < n_particles; j++)
+        for (int j = 0; j < i; j++)
         {
 			if(i != j)
 			{
@@ -137,12 +139,19 @@ for (int i = 0; i < n_particles; i ++)
 				}
 				else
 				{
-					dij = std::sqrt(dij);
-					dij = 10.0 / (dij * dij * dij);
+					__m256 x_vec = _mm256_set1_ps(dij);
+					__m256 inv_sqrt_x_vec = _mm256_rsqrt_ps(x_vec);
+					inv_sqrt_x_vec = _mm256_mul_ps(inv_sqrt_x_vec,_mm256_mul_ps(inv_sqrt_x_vec,inv_sqrt_x_vec));
+					float inv_sqrt_x = _mm256_cvtss_f32(inv_sqrt_x_vec);
+					dij = 10.0 * inv_sqrt_x;
 				}
 				accelerationsx[i] += diffx * dij * initstate.masses[j];
 				accelerationsy[i] += diffy * dij * initstate.masses[j];
 				accelerationsz[i] += diffz * dij * initstate.masses[j];
+
+				accelerationsx[j] -= diffx * dij * initstate.masses[i];
+				accelerationsy[j] -= diffy * dij * initstate.masses[i];
+				accelerationsz[j] -= diffz * dij * initstate.masses[i];
 			}
 		}
 	}
@@ -156,57 +165,6 @@ for (int i = 0; i < n_particles; i ++)
 		particles.y[i] += velocitiesy   [i] * 0.1f;
 		particles.z[i] += velocitiesz   [i] * 0.1f;
 	}
-// #elif STRATEGY == STD_ATOMIC
-
-// std::atomic<float> sumx{0ull};
-// std::atomic<float> sumy{0ull};
-// std::atomic<float> sumz{0ull};
-// for (int i = 0; i < n_particles; i ++)
-//     {
-//         #pragma omp parallel for
-//         for (int j = 0; j <n_particles; j++)
-//         {
-//             sumx = 0.0;
-//             sumy = 0.0;
-//             sumz = 0.0;
-// 			if(i != j)
-// 			{
-// 				const float diffx = particles.x[j] - particles.x[i];
-// 				const float diffy = particles.y[j] - particles.y[i];
-// 				const float diffz = particles.z[j] - particles.z[i];
-
-// 				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
-
-// 				if (dij < 1.0)
-// 				{
-// 					dij = 10.0;
-// 				}
-// 				else
-// 				{
-// 					dij = std::sqrt(dij);
-// 					dij = 10.0 / (dij * dij * dij);
-// 				}
-
-// 				sumx += diffx * dij * initstate.masses[j];
-// 				sumy += diffy * dij * initstate.masses[j];
-// 				sumz += diffz * dij * initstate.masses[j];
-// 			}
-// 		}
-//         	accelerationsx[i] = sumx;
-// 			accelerationsy[i] = sumy;
-// 			accelerationsz[i] = sumz;
-// 	}
-//     #pragma omp parallel for
-// 	for (int i = 0; i < n_particles; i++)
-// 	{
-// 		velocitiesx[i] += accelerationsx[i] * 2.0f;
-// 		velocitiesy[i] += accelerationsy[i] * 2.0f;
-// 		velocitiesz[i] += accelerationsz[i] * 2.0f;
-// 		particles.x[i] += velocitiesx   [i] * 0.1f;
-// 		particles.y[i] += velocitiesy   [i] * 0.1f;
-// 		particles.z[i] += velocitiesz   [i] * 0.1f;
-// 	}
-
 
 #endif
 
